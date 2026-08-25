ea2183dbd783784dfe4f15a3aa4633366ef7dd49
Tsuwagyu <runmikeyrun2@gmail.com>
2026-08-25 17:07:55 -0400

    add node class

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// using class bc it when we use new Node() its obvious that we r making a specific type of obj
+class Node {
+
+    // every node will start with these defaults, each node will remember val stored in the node, reference to left / right child
+    constructor(data) {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+    }
+
+    
+}
