cab79278fa5ac155a84c8c31988ec6305486f0fa
Tsuwagyu <runmikeyrun2@gmail.com>
2026-08-25 19:16:52 -0400

    add tree class and start buildTree method

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ class Node {
 
     
 }
+
+class Tree {
+    // arr is the parameter that receives the array
+    constructor(arr) {
+        // without root being set to null, js skips it as undefined
+        // with this.root set to null, its value is considered nothing but it exists
+        this.root = null;
+    }
+}
+
+function buildTree(array) {
+
+    let arr = array;
+
+    function findDupes {
+        // filter checks if the index of the first occurence is matching with the current index
+        // indexOf finds the position of the first occurence of a specified value within arr or string
+        arr.filter((item, index) => arr.indexOf(item) === index);
+    }
+
+
+}
