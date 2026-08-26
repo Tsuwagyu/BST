25ce66050c029183cd1ff7e2ea20a05494f7341e
Tsuwagyu <runmikeyrun2@gmail.com>
2026-08-26 16:30:14 -0400

    fix: rename function, filtered array is stored and modified correctly

--- main.js
+++ main.js
@@ -25,19 +25,22 @@ function buildTree(array) {
 
     let arr = array;
 
-    function findDupes {
+    function removeDupesAndSort() {
 
         // filter checks if the index of the first occurence is matching with the current index
         // indexOf finds the position of the first occurence of a specified value within arr or string
-        arr.filter((item, index) => arr.indexOf(item) === index);
+        let noDupes = arr.filter((item, index) => arr.indexOf(item) === index);
         //.sort() method alone bc it converts value to string and sorts lexographically
         //.sort with a compare function, if value comes out negative then a comes before b, if the value comes out positive a comes after b
-        let sortedArr = [...arr].sort((a, b) => a - b);
+        let sortedArr = [...noDupes].sort((a, b) => a - b);
 
+        return sortedArr;
 
     }
 
-    
+
+
+
 
 
 }
